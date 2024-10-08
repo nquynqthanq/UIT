1798ebbb43a7f197654ac4f137a1313f2c181661
Nguyen Quoc Thang <nguyenqthangwork@gmail.com>
2024-10-08 20:03:53 +0700

    BT4

---
+++ BT4/PRODUCT_MANAGEMENT/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using QuanLySanPham.Models;
+
+namespace QuanLySanPham.Controllers;
+
+public class CatalogController : Controller
+{
+    private readonly MyDbContext _context;
+
+    public CatalogController(MyDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        List<Catalog> dsCatalogs = _context.Catalogs.ToList();
+
+        return View(dsCatalogs);
+    }
+}
---
+++ BT4/PRODUCT_MANAGEMENT/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using QuanLySanPham.Models;
+
+namespace QuanLySanPham.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly MyDbContext _context;
+
+    public ProductController(MyDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        List<Product> dsCatalogs = _context.Products.ToList();
+
+        return View(dsCatalogs);
+    }
+}
